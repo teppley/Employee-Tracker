da9fcf234cb12625a2fe46dfb992451018489685
Tyler Eppley <teppley300@gmail.com>
2023-04-30 12:21:02 -0400

    added prompt and answers menu

--- server.js
+++ server.js
@@ -30,3 +30,54 @@ db.connect(err => {
 
 
 // Start the prompt functions
+function startPrompt() {
+    inquirer.prompt({
+            type: 'list',
+            name: 'menu',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Update An Employee Manager', 'Delete Department', 'Delete Role', 'Delete Employee', 'Quit'], 
+
+    }).then( answer => {
+        switch (answer.menu) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add A Department':
+                addDepartment();
+                break;
+            case 'Add A Role':
+                addRole();
+                break;
+            case 'Add An Employee':
+                addEmployee();
+                break;
+            case 'Update An Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'Update An Employee Manager':
+                updateEmployeeManager();
+                break;
+            case 'Delete Department':
+                deleteDepartment();
+                break;
+            case 'Delete Role':
+                deleteRole();
+                break;
+            case 'Delete Employee':
+                deleteEmployee();
+                break;
+                case 'Quit':
+                console.log('Goodbye!');
+                process.exit(0);
+            default:
+                console.log(`Invalid action: ${answer.menu}`);
+                startPrompt();
+        }
+    })
+ };
